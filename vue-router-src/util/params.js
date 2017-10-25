50a67fd00dfcd16d575386449ed2dffc523dbb0e
answershuto <905307789@qq.com>
2017-10-25 20:14:39 +0800

    add vue-router-src

---
+++ vue-router-src/util/params.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import { warn } from './warn'
+import Regexp from 'path-to-regexp'
+
+// $flow-disable-line
+const regexpCompileCache: {
+  [key: string]: Function
+} = Object.create(null)
+
+export function fillParams (
+  path: string,
+  params: ?Object,
+  routeMsg: string
+): string {
+  try {
+    const filler =
+      regexpCompileCache[path] ||
+      (regexpCompileCache[path] = Regexp.compile(path))
+    return filler(params || {}, { pretty: true })
+  } catch (e) {
+    if (process.env.NODE_ENV !== 'production') {
+      warn(false, `missing param for ${routeMsg}: ${e.message}`)
+    }
+    return ''
+  }
+}
